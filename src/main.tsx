0cf1c1579e2797a79eb8073100992206ef5537b4
alcides Paz <alcidesnp@hotmail.com>
2023-08-23 17:02:34 -0300

    Aula1

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Header from './components/Header'
+import Footer from './components/Fotter'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Header/>
+    <Footer/>
+  </React.StrictMode>,
+)
